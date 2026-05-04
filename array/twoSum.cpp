59f4ff8fd2fdccfaa8aa63902ec619c367f6567d
raaihansvg <rlazuardi163@gmail.com>
2026-05-04 16:50:15 +0700

    leetcode twoSum final

--- array/twoSum.cpp
+++ array/twoSum.cpp
@@ -1,13 +1,17 @@
 #include <vector>
 using namespace std;
 
-vector<int> twoSum(vector<int>& arr, int target){
-    for(int i = 0; i < arr.size(); i++){
-        for(int j = i + 1; j < arr.size(); j++){
-            if(arr[i] + arr[j] == target){
-                return {i,j};
+
+class solution{
+    public:
+    vector<int> twoSum(vector<int>& arr, int target){
+        for(int i = 0; i < arr.size(); i ++){
+            for(int j = i + 1; j < arr.size(); j++){
+                if(arr[i] + arr[j] == target){
+                    return {i,j};
+                }
             }
         }
+        return{};
     }
-    return {};
-}
+};
