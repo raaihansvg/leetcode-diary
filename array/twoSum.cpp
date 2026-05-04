1b98052f2ab4e9487ca158987f463c638bd10342
raaihansvg <rlazuardi163@gmail.com>
2026-05-04 16:37:42 +0700

    leetcode arrat easytwo sum

--- array/twoSum.cpp
+++ array/twoSum.cpp
@@ -1,11 +1,11 @@
 #include <vector>
 using namespace std;
 
-vector<int> twoSum(vector<int>& nums, int target){
-    for(int i = 0; i < nums.size(); i++){
-        for(int j = i + 1; j < nums.size(); j++){
-            if(nums[i] + nums[j] == target){
-                return {i + j};
+vector<int> twoSum(vector<int>& arr, int target){
+    for(int i = 0; i < arr.size(); i++){
+        for(int j = i + 1; j < arr.size(); j++){
+            if(arr[i] + arr[j] == target){
+                return {i,j};
             }
         }
     }
